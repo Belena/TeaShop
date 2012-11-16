bdda075ac2e8697c71a3320d301f10a679b494a1
Elena Burnos <elena.burnos@gmail.com>
2012-11-16 20:23:52 +0000

    first-first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeaShop::Application.config.secret_token = '1da01cf1cab0d9871f5db750edfd8e22238132a14da2fa99a0c7d43896adf50a4d758020621cce1b1166dd237f0a5f29679f82ec144c65e6abb731cea9769d8b'
